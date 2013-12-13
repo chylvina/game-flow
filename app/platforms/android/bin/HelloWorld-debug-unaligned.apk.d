b400374a089f01326f809b6b3ee7c53eda66a7c9
chylvina <chylvina@gmail.com>
2013-12-13 21:54:27 +0800

    re structure

---
+++ app/platforms/android/bin/HelloWorld-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+D:\workspace\GitHub\game-flow\platforms\android\bin\HelloWorld-debug-unaligned.apk : \
+D:\workspace\GitHub\game-flow\platforms\android\bin\HelloWorld.ap_ \
+D:\workspace\GitHub\game-flow\platforms\android\bin\classes.dex \
+D:\workspace\GitHub\game-flow\platforms\android\libs\cordova-3.2.0.jar \
