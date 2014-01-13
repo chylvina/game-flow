bf062463e76a1848688ec9e3bf774644cc457635
xiongliang.xl <xiongliang.xl@alibaba-inc.com>
2014-01-13 16:20:51 +0800

    recon

--- app/platforms/android/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-D:\workspace\GitHub\game-flow\app\platforms\android\bin\AndroidManifest.xml : \
-D:\workspace\GitHub\game-flow\app\platforms\android\AndroidManifest.xml \
--- app/platforms/android/gen/R.java.d
+++
@@ -1,9 +0,0 @@
-D:\workspace\GitHub\game-flow\app\platforms\android\gen\com\phonegap\hello_world\R.java \
- : D:\workspace\GitHub\game-flow\app\platforms\android\res\drawable\icon.png \
-D:\workspace\GitHub\game-flow\app\platforms\android\res\drawable-hdpi\icon.png \
-D:\workspace\GitHub\game-flow\app\platforms\android\res\drawable-ldpi\icon.png \
-D:\workspace\GitHub\game-flow\app\platforms\android\res\drawable-mdpi\icon.png \
-D:\workspace\GitHub\game-flow\app\platforms\android\res\drawable-xhdpi\icon.png \
-D:\workspace\GitHub\game-flow\app\platforms\android\res\values\strings.xml \
-D:\workspace\GitHub\game-flow\app\platforms\android\res\xml\config.xml \
-D:\workspace\GitHub\game-flow\app\platforms\android\bin\AndroidManifest.xml \
