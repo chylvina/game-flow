01a1f56c697a754af7ea90689d0425dd84a5c2e6
xiongliang.xl <xiongliang.xl@alibaba-inc.com>
2014-01-13 18:08:48 +0800

    next to enable touch in html

--- app/platforms/android/src/main/java/com/chylvina/android/gameflow/MainActivity.java
+++ app/platforms/android/src/main/java/com/chylvina/android/gameflow/MainActivity.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
@@ -25,8 +27,11 @@ public class MainActivity extends Activity {
         mWebView = (WebView) findViewById(R.id.webview);
         mWebView.getSettings().setJavaScriptEnabled(true);
         mWebView.getSettings().setRenderPriority(WebSettings.RenderPriority.HIGH);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN)
+        mWebView.getSettings().setDomStorageEnabled(true);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
             mWebView.getSettings().setAllowUniversalAccessFromFileURLs(true);
+        }
 
         mWebView.setWebChromeClient(new WebChromeClient() {
 
@@ -46,8 +51,8 @@ public class MainActivity extends Activity {
                     @Override
                     public void run() {
                         // TODO Auto-generated method stub
-                        //Intent intent = new Intent(MainActivity.this, ActivityOther.class);
-                        //startActivity(intent);
+                        Intent intent = new Intent(MainActivity.this, ActivityOther.class);
+                        startActivity(intent);
                     }
                 });
             }
